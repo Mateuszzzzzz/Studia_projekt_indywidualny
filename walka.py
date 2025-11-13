168a2de572fe3a47096841bb943257bbaa9fd8d6
Mateuszzzzz <mat.tucholski@gmail.com>
2025-11-13 22:08:13 +0100

    Revert "Commit pierwszy"
    This reverts commit 00a864faa94f000eab26177f3d366fc3627b6230.

--- walka.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def draw_walka(screen, WIDTH, HEIGHT, font_path):
-    font = pygame.font.Font(font_path, 28)
-    text = font.render("Tu będzie walka", True, (255, 255, 255))
-    screen.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
-
