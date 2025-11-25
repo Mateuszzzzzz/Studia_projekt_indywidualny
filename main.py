8c44711ffc8fc26acc331ac5e6fbc5f4cd0aa7ff
Mateuszzzzz <mat.tucholski@gmail.com>
2025-11-25 03:04:22 +0100

    nowa wersja

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ pygame.display.set_caption("Lista zadań")
 #Kolory uzywane w apce
 BIALY = (255, 255, 255)
 CZARNY = (0, 0, 0)
-FIOLET = (96, 43, 96)
-ZIELONY = (81, 206, 49)
+FIOLET = (39, 46, 42)
+FIOLET2 = (42, 73, 61)
+ZIELONY = (29, 162, 84)
+ZIELONY2 = (20, 114, 60)
 SZARY = (150, 150, 150)
-FIOLET2 = (200, 191, 231)
 PRZYCIEMNIENIE = (0, 0, 0, 150)
-PRZYCISK_BG = (180, 180, 180)
-PRZYCISK_HOVER = (203, 137, 203)
+PRZYCISK_BG = (160, 160, 160)
+PRZYCISK_HOVER = (29, 162, 84)
 AKTYWNE_POLE = (0, 255, 0)
-PLACEHOLDER_COLOR = (160, 160, 160)
+JASNY_SZARY = (160, 160, 160)
 BRAK_TYTULU_COLOR = (255, 0, 0)
 
 #Ustawienia dla czcionki (moze potem zmienic te vt323 na jakas autorska jak starczy czasu?)
-MAX_FONT_SIZE = 28
+MAX_FONT_SIZE = 23
 MIN_FONT_SIZE = 12
 font_path = os.path.join(os.path.dirname(__file__), "Czcionka", "VT323-Regular.ttf")
 
@@ -43,7 +44,8 @@ cur.execute('''CREATE TABLE IF NOT EXISTS zadania (
     Opis_zadania TEXT,
     Nagroda INTEGER DEFAULT 0,
     Data_dodania TEXT,
-    Czy_zaliczone INTEGER DEFAULT 0
+    Czy_zaliczone INTEGER DEFAULT 0,
+    Data_do_końca TEXT
 )''')
 #staty gracza w bazie
 cur.execute('''CREATE TABLE IF NOT EXISTS gracz (
@@ -162,11 +164,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button in (4, 5):
             if active_tab == "Zadania" and not show_input and not show_details:
                 item_h = 35
                 gap = 10
-                content_height = tab_height + 10 + len(tasks) * (item_h + gap)
+                aktywne = [t for t in tasks if t["completed"] == 0]
+                wykonane = [t for t in tasks if t["completed"] == 1]
+                content_height = (
+                        10 +
+                        30 + 15 +
+                        len(aktywne) * (item_h + gap) +
+                        20 +
+                        30 + 15 +
+                        len(wykonane) * (item_h + gap) +
+                        40
+                )
                 visible_height = HEIGHT - (tab_height + 10)
                 max_scroll = max(0, content_height - visible_height)
                 if event.button == 4:
@@ -306,33 +319,67 @@ while running:
         y = tab_height + 10 - scroll_y
         item_h = 35
         gap = 10
-        for task in tasks:
-            color = BIALY if task["completed"] else CZARNY
-            text_surface = task["font"].render(task["text"], True, color)
-            border_rect = pygame.Rect(9, y, WIDTH - 18, item_h)
-            if task["completed"]:
-                pygame.draw.rect(screen, ZIELONY, border_rect, border_radius=8)
-            else:
-                pygame.draw.rect(screen, FIOLET2, border_rect, border_radius=8)
-            pygame.draw.rect(screen, BIALY, border_rect, width=2, border_radius=8)
-            screen.blit(text_surface, (18, border_rect.y + 5))
+        naglowek_font = pygame.font.Font(font_path, 22)
+
+        aktywne = [t for t in tasks if t["completed"] == 0]
+        wykonane = [t for t in tasks if t["completed"] == 1]
+
+        task_positions = []
+
+        start_y_aktywne = y+5
+
+        text = naglowek_font.render("ZADANIA AKTYWNE", True, BIALY)
+        screen.blit(text, (30, y))
+        y += text.get_height() + 8
+
+        start_y_aktywne = y + 5
+        item_h = 35
+        gap = 10
+        outer_height = len(aktywne) * (item_h + gap) - gap + 20
+        outer_rect_aktywne = pygame.Rect(20, start_y_aktywne - 10, WIDTH - 40, outer_height)
+
+        pygame.draw.rect(screen, ZIELONY2, outer_rect_aktywne, border_radius=12)
+        pygame.draw.rect(screen, BIALY, outer_rect_aktywne, 2, border_radius=12)
+
+        y = start_y_aktywne
+        for task in aktywne:
+            text_surface = task["font"].render(task["text"], True, CZARNY)
+            border_rect = pygame.Rect(30, y, WIDTH - 60, item_h)
+            pygame.draw.rect(screen, BIALY, border_rect, border_radius=8)
+            pygame.draw.rect(screen, FIOLET, border_rect, 2, border_radius=8)
+            screen.blit(text_surface, (border_rect.x + 10, border_rect.y + 5))
             task_positions.append((border_rect, task["id"]))
             y += item_h + gap
 
-        content_height = tab_height + 10 + len(tasks) * (item_h + gap)
-        visible_height = HEIGHT - (tab_height + 10)
-        max_scroll = max(0, content_height - visible_height)
-        if scroll_y < 0:
-            scroll_y = 0
-        if scroll_y > max_scroll:
-            scroll_y = max_scroll
+        y += 20
+        start_y_wykonane = y
+
+        text = naglowek_font.render("ZADANIA WYKONANE", True, BIALY)
+        screen.blit(text, (30, y))
+        y += text.get_height() + 15
+
+        item_h = 35
+        gap = 10
+        outer_height = len(wykonane) * (item_h + gap) - gap + 20
+        outer_rect_wykonane = pygame.Rect(20, y - 10, WIDTH - 40, outer_height)
+        pygame.draw.rect(screen, ZIELONY2, outer_rect_wykonane, border_radius=12)
+        pygame.draw.rect(screen, BIALY, outer_rect_wykonane, 2, border_radius=12)
+
+        for task in wykonane:
+            text_surface = task["font"].render(task["text"], True, CZARNY)
+            border_rect = pygame.Rect(30, y, WIDTH - 60, item_h)
+            pygame.draw.rect(screen, ZIELONY, border_rect, border_radius=8)
+            pygame.draw.rect(screen, BIALY, border_rect, 2, border_radius=8)
+            screen.blit(text_surface, (border_rect.x + 10, border_rect.y + 5))
+            task_positions.append((border_rect, task["id"]))
+            y += item_h + gap
 
     screen.set_clip(None)
 
     for i, tab in enumerate(tabs):
         tab_rect = pygame.Rect(i * tab_width, 0, tab_width, tab_height)
         if tab == active_tab:
-            pygame.draw.rect(screen, FIOLET2, tab_rect)
+            pygame.draw.rect(screen, ZIELONY, tab_rect)
         elif tab_rect.collidepoint(mouse_pos):
             pygame.draw.rect(screen, PRZYCISK_HOVER, tab_rect)
         else:
@@ -342,7 +389,6 @@ while running:
         screen.blit(text_surface, (tab_rect.centerx - text_surface.get_width() // 2,
 
                                    tab_rect.centery - text_surface.get_height() // 2))
-    #Formularz dodawnia nowych zadan
     if active_tab == "Zadania":
         current_button_img = button_hover_image if button_rect.collidepoint(mouse_pos) and not show_details else button_image
         if show_input:
@@ -376,7 +422,7 @@ while running:
             pygame.draw.rect(screen, (245, 245, 245), title_rect, border_radius=8)
             pygame.draw.rect(screen, title_border_color, title_rect, 2, border_radius=8)
             font_title = get_fitting_font(input_title, title_rect.width)
-            color_title = CZARNY if input_title != "Tytuł..." else PLACEHOLDER_COLOR
+            color_title = CZARNY if input_title != "Tytuł..." else JASNY_SZARY
             screen.blit(font_title.render(input_title, True, color_title),
                         (title_rect.x + 10, title_rect.y + (title_rect.height - font_title.size(input_title)[1]) // 2))
             counter_surface = counter_font.render(f"{len(input_title)}/{MAX_TITLE_CHARS}", True, SZARY)
@@ -389,7 +435,7 @@ while running:
             pygame.draw.rect(screen, (245, 245, 245), desc_rect, border_radius=8)
             pygame.draw.rect(screen, desc_border_color, desc_rect, 2, border_radius=8)
             font_desc = get_fitting_font(input_desc, desc_rect.width, max_font=20)
-            color_desc = CZARNY if input_desc != "Opis..." or active_field == "desc" else PLACEHOLDER_COLOR
+            color_desc = CZARNY if input_desc != "Opis..." or active_field == "desc" else JASNY_SZARY
 
             desc_lines = wrap_text(input_desc, font_desc, desc_rect.width - 15)
             y_text = desc_rect.y + 6
@@ -407,7 +453,7 @@ while running:
             pygame.draw.rect(screen, (245, 245, 245), reward_rect, border_radius=8)
             pygame.draw.rect(screen, reward_border_color, reward_rect, 2, border_radius=8)
             font_reward = get_fitting_font(input_reward, reward_rect.width, max_font=16)
-            color_reward = CZARNY if input_reward != "0" or active_field == "reward" else PLACEHOLDER_COLOR
+            color_reward = CZARNY if input_reward != "0" or active_field == "reward" else JASNY_SZARY
             screen.blit(font_reward.render(input_reward, True, color_reward),
                         (reward_rect.x + 10, reward_rect.y + (reward_rect.height - font_reward.size(input_reward)[1]) // 2))
             counter_surface = counter_font.render(f"Max: {MAX_REWARD}", True, SZARY)
@@ -465,8 +511,6 @@ while running:
             screen.blit(date, (details_rect.x + 20, y_offset + 50))
 
             close_font = pygame.font.Font(font_path, 18)
-            close_text = close_font.render("Zamknij", True, BIALY)
-
             if selected_task[5] == 0:
                 button_width = 100
                 button_height = 30
@@ -476,13 +520,16 @@ while running:
                 done_rect = pygame.Rect(start_x, details_rect.bottom - 40, button_width, button_height)
                 close_rect = pygame.Rect(start_x + button_width + spacing, details_rect.bottom - 40, button_width, button_height)
 
-                done_color = FIOLET2 if done_rect.collidepoint(mouse_pos) else SZARY
-                close_color = FIOLET2 if close_rect.collidepoint(mouse_pos) else SZARY
+                done_color = ZIELONY if done_rect.collidepoint(mouse_pos) else SZARY
+                done_font_color = CZARNY if done_rect.collidepoint(mouse_pos) else BIALY
+                close_color = ZIELONY if close_rect.collidepoint(mouse_pos) else SZARY
+                close_font_color = CZARNY if close_rect.collidepoint(mouse_pos) else BIALY
+                close_text = close_font.render("Zamknij", True, close_font_color)
 
                 pygame.draw.rect(screen, done_color, done_rect, border_radius=8)
                 pygame.draw.rect(screen, close_color, close_rect, border_radius=8)
-
-                done_text = close_font.render("Wykonane", True, BIALY)
+                close_text = close_font.render("Zamknij", True, close_font_color)
+                done_text = close_font.render("Wykonane", True, done_font_color)
                 screen.blit(done_text, (done_rect.centerx - done_text.get_width() // 2,
                                         done_rect.centery - done_text.get_height() // 2))
                 screen.blit(close_text, (close_rect.centerx - close_text.get_width() // 2,
@@ -490,6 +537,7 @@ while running:
             else:
                 close_rect = pygame.Rect(details_rect.centerx - 50, details_rect.bottom - 40, 100, 30)
                 close_color = FIOLET2 if close_rect.collidepoint(mouse_pos) else SZARY
+                close_text = close_font.render("Zamknij", True, BIALY)
                 pygame.draw.rect(screen, close_color, close_rect, border_radius=8)
                 screen.blit(close_text, (close_rect.centerx - close_text.get_width() // 2,
                                          close_rect.centery - close_text.get_height() // 2))
